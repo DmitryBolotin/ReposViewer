5e118a700aa13e95f0c57308894c8e4881cb0076
Dmitry Bolotin <dbolotin@mail.ru>
2016-02-10 00:05:13 +0300

    added missed scripts

---
+++ scripts/libs/userscript/userScript.js
@@ -0,0 +1,12 @@
+(function(){
+    define([
+        'jQuery',
+        'bootstrap'
+    ], function($){
+
+// Highlight tooltips pn templates
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    });
+})();
